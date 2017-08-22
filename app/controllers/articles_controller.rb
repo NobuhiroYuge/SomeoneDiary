409252751c3e01dc49d541d59088d808309f4615
nyuge <nyuge@chemsys.t.u-tokyo.ac.jp>
2017-08-22 19:21:29 +0900

    [refine] add CRUD methods

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,9 +1,46 @@
 class ArticlesController < ApplicationController
-  def index
-  end
+  before_action :set_article, only:[:show, :edit, :update, :destroy]
 
   def new
+    # this instance is a dummy !
     @article = Article.new
   end
+
+  def index
+    @articles = Article.all 
+  end
+
+  def show
+  end
+
+  def create
+    # Article.create is also vaild
+    @article = Article.new(article_params)
+    @article.save
+    redirect_to '/articles'
+    # redirect_to @article
+  end
+
+  def edit
+  end
+
+  def update
+    @article.update(article_params)
+    redirect_to @article
+  end
+
+  def destroy
+    @article.destroy
+    redirect_to '/articles'
+  end
+
+  private
+    def set_article
+      @article = Article.find(params[:id])
+    end
+
+    def article_params
+      params.require(:article).permit(:title, :body, :author)
+    end
 
 end  # end 'ArticlesController' class
