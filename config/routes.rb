a6a9056a63ab0523f0a8ae42ac99894ce42f65af
nyuge <nyuge@chemsys.t.u-tokyo.ac.jp>
2017-08-29 19:54:14 +0900

    [bugfix] enable to sign_out by url destination (according to http://qiita.com/colorrabbit/items/5545fce7e5cd4e494396 issues)

--- config/routes.rb
+++ config/routes.rb
@@ -2,20 +2,40 @@ Rails.application.routes.draw do
   get 'posts/index'
 
   devise_for :users
-  root 'posts#index' 
+  root 'articles#new'
 
-  # class HomeController
-  # root 'home#top'  # This is the assignment in the 1st class.
+  # For devise7s sign_out activation 
+  devise_scope :user do
+    get '/users/sign_out' => 'devise/sessions#destroy'
+  end
 
-  # class ArticlesController
-  #     articles GET    /articles(.:format)          articles#index
-  #              POST   /articles(.:format)          articles#create
-  #  new_article GET    /articles/new(.:format)      articles#new
-  # edit_article GET    /articles/:id/edit(.:format) articles#edit
-  #      article GET    /articles/:id(.:format)      articles#show
-  #              PATCH  /articles/:id(.:format)      articles#update
-  #              PUT    /articles/:id(.:format)      articles#update
-  #              DELETE /articles/:id(.:format)      articles#destroy
   resources :articles
 
+  #                   Prefix Verb   URI Pattern                    Controller#Action
+  #              posts_index GET    /posts/index(.:format)         posts#index
+  #         new_user_session GET    /users/sign_in(.:format)       devise/sessions#new
+  #             user_session POST   /users/sign_in(.:format)       devise/sessions#create
+  #     destroy_user_session DELETE /users/sign_out(.:format)      devise/sessions#destroy
+  #        new_user_password GET    /users/password/new(.:format)  devise/passwords#new
+  #       edit_user_password GET    /users/password/edit(.:format) devise/passwords#edit
+  #            user_password PATCH  /users/password(.:format)      devise/passwords#update
+  #                          PUT    /users/password(.:format)      devise/passwords#update
+  #                          POST   /users/password(.:format)      devise/passwords#create
+  # cancel_user_registration GET    /users/cancel(.:format)        devise/registrations#cancel
+  #    new_user_registration GET    /users/sign_up(.:format)       devise/registrations#new
+  #   edit_user_registration GET    /users/edit(.:format)          devise/registrations#edit
+  #        user_registration PATCH  /users(.:format)               devise/registrations#update
+  #                          PUT    /users(.:format)               devise/registrations#update
+  #                          DELETE /users(.:format)               devise/registrations#destroy
+  #                          POST   /users(.:format)               devise/registrations#create
+  #                     root GET    /                              articles#new
+  #           users_sign_out GET    /users/sign_out(.:format)      devise/sessions#destroy
+  #                 articles GET    /articles(.:format)            articles#index
+  #                          POST   /articles(.:format)            articles#create
+  #              new_article GET    /articles/new(.:format)        articles#new
+  #             edit_article GET    /articles/:id/edit(.:format)   articles#edit
+  #                  article GET    /articles/:id(.:format)        articles#show
+  #                          PATCH  /articles/:id(.:format)        articles#update
+  #                          PUT    /articles/:id(.:format)        articles#update
+  #                          DELETE /articles/:id(.:format)        articles#destroy
 end
